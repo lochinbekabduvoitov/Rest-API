b1180ce1d292d2fbf61c4a4984951181677cedb1
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-01-13 17:01:58 +0500

    Api Sclass Controller

--- app/Models/Sclass.php
+++ app/Models/Sclass.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Sclass extends Model
 {
     use HasFactory;
+
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'class_name'
+    ];
 }
