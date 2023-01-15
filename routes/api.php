e9e35801f64c126e5592a41c988504bb57784c14
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-01-15 09:45:25 +0500

    student controller model and migration

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\SectionController;
+use App\Http\Controllers\Api\StudentController;
 
 Route::apiResource('class', SclassController::class);
 Route::apiResource('subject', SubjectController::class);
 Route::apiResource('section', SectionController::class);
+Route::apiResource('student', StudentController::class);
 
