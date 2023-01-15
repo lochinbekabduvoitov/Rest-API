25e1c207eece42bb1b32dbf46e08a1d55ca32e96
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-01-15 11:28:07 +0500

    jwt auth install

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,22 @@ use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\SectionController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\AuthController;
 
 Route::apiResource('class', SclassController::class);
 Route::apiResource('subject', SubjectController::class);
 Route::apiResource('section', SectionController::class);
 Route::apiResource('student', StudentController::class);
 
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', AuthController::class);
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
