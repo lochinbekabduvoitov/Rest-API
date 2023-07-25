4114667da1cd8dab3cb11d18f939fbe02528f9af
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-25 10:21:25 +0500

    update project

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,19 @@
 <?php
 
-use App\Http\Controllers\Api\SclassController;
-use App\Http\Controllers\Api\SubjectController;
-use App\Http\Controllers\Api\SectionController;
-use App\Http\Controllers\Api\StudentController;
-use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::apiResource('class', SclassController::class);
-Route::apiResource('subject', SubjectController::class);
-Route::apiResource('section', SectionController::class);
-Route::apiResource('student', StudentController::class);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-Route::group([
-
-    'prefix' => 'auth'
-
-], function () {
-
-    Route::post('login',[ AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class ,'logout']);
-    Route::post('refresh', [AuthController::class ,'refresh']);
-    Route::post('me',[ AuthController::class , 'me']);
-    Route::post('register',[ AuthController::class , 'register']);
-
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
