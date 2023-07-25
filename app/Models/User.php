ffb22695f84b20f45ebeed099da4c2835acac32a
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-25 12:28:13 +0500

    create factory and seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone',
         'email',
         'password',
     ];
@@ -42,4 +44,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::apiResources([
+    'categories'=> CategoryController::class
+]);
