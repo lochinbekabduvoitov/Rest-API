0f324d25060891cb3791f0df41562e4de4470089
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-29 09:36:47 +0500

    Auth with laravel sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CategoryProductController;
 use App\Http\Controllers\ProductController;
@@ -11,6 +12,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login',[AuthController::class,"login"]);
+Route::post('register',[AuthController::class,"register"]);
 
 
 Route::apiResources([
