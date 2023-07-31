18baeb313d6b99e98505b26e62868305fe7e8683
Lochinbek Abduvoitov <lochinbekabduvoitov8@gmail.com>
2023-07-31 17:47:19 +0500

    Favorite

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -13,11 +13,13 @@ class FavoriteController extends Controller
         $this->middleware('auth:sanctum');
     }
 
+
     public function index()
     {
         return auth()->user()->favorites()->paginate(20);
     }
 
+    
     public function store(Request $request): JsonResponse
     {
         auth()->user()->favorites()->attach($request->product_id);
@@ -31,6 +33,7 @@ class FavoriteController extends Controller
     public function destroy($favorite_id)
     {
         if (auth()->user()->hasFavorite($favorite_id)) {
+
             auth()->user()->favorites()->detach($favorite_id);
 
             return response()->json([
